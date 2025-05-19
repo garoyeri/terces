5d97e29f651d860ef4b1a9dff148ec139cface0c
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2025-05-19 18:16:38 -0500

    Add TimeProvider abstraction to allow for time-traveling during testing.

--- src/Terces/InMemorySecretStore.cs
+++ src/Terces/InMemorySecretStore.cs
@@ -2,14 +2,19 @@
 
 namespace Terces;
 
-public class InMemorySecretStore : ISecretStore
+public class InMemorySecretStore(TimeProvider time) : ISecretStore
 {
     private const string StoreId = "in-memory-store";
 
     private record SecretEntry(SecretInfo Info, string SecretValue);
 
     readonly ConcurrentDictionary<string, SecretEntry> _secrets = new();
-    
+    private readonly TimeProvider _time = time;
+
+    public InMemorySecretStore() : this(TimeProvider.System)
+    {
+    }
+
     public Task<SecretInfo?> GetSecretAsync(string name, CancellationToken cancellationToken)
     {
         var result = _secrets.GetValueOrDefault(name);
@@ -21,13 +26,13 @@ public class InMemorySecretStore : ISecretStore
     {
         var result = _secrets.AddOrUpdate(name,
             s => new SecretEntry(
-                new SecretInfo(name, name, contentType, true, DateTimeOffset.UtcNow, expiresOn, DateTimeOffset.UtcNow,
+                new SecretInfo(name, name, contentType, true, _time.GetUtcNow(), expiresOn, _time.GetUtcNow(),
                     StoreId, "1"),
                 value
             ),
             (s, entry) => new SecretEntry(
                 new SecretInfo(name, name, contentType, entry.Info.Enabled, entry.Info.CreatedOn,
-                    expiresOn, DateTimeOffset.UtcNow, entry.Info.StoreId, entry.Info.Version),
+                    expiresOn, _time.GetUtcNow(), entry.Info.StoreId, entry.Info.Version),
                 value
             )
         );
