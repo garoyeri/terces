9f1b65ea877f9b6257678458b3a5383abd367602
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2025-05-21 22:04:27 -0500

    Add result records

---
+++ src/Terces/InspectionResult.cs
@@ -0,0 +1,41 @@
+﻿namespace Terces;
+
+/// <summary>
+/// Represents the result of a secret inspection.
+/// </summary>
+/// <remarks>
+/// This record includes information about the inspection, such as its name, strategy,
+/// optionally associated secret metadata, additional notes, and an optional resource identifier.
+/// </remarks>
+public record InspectionResult
+{
+    /// <summary>
+    /// Gets the secret name associated with the inspection result.
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Gets or sets the rotation strategy associated with the inspection result.
+    /// </summary>
+    public required string Strategy { get; init; }
+
+    /// <summary>
+    /// Represents metadata about a secret associated with the inspection result.
+    /// </summary>
+    public SecretInfo? Secret { get; init; } = null;
+
+    /// <summary>
+    /// Gets or initializes informational or descriptive notes associated with the inspection result.
+    /// </summary>
+    /// <remarks>
+    /// This property is designed to store additional context, comments, or metadata
+    /// that provide further insights or explanations regarding the inspection result.
+    /// By default, this property is initialized to an empty string.
+    /// </remarks>
+    public string Notes { get; init; } = "";
+
+    /// <summary>
+    /// Represents the identifier of a specific resource associated with the inspection result.
+    /// </summary>
+    public string? ResourceId { get; init; } = null;
+}
---
+++ src/Terces/RotationResult.cs
@@ -0,0 +1,25 @@
+﻿namespace Terces;
+
+/// <summary>
+/// Represents the result of a rotation operation, containing details
+/// about the rotation status, the target of the operation, and any
+/// additional notes.
+/// </summary>
+public record RotationResult
+{
+    /// <summary>
+    /// Gets the name associated with the rotation result.
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Indicates whether the entity associated with the result was successfully rotated.
+    /// </summary>
+    public required bool WasRotated { get; init; }
+
+    /// <summary>
+    /// Provides additional information or remarks about the rotation outcome.
+    /// This property can be used to store any notes or comments relevant to the specific result.
+    /// </summary>
+    public string Notes { get; init; } = "";
+}
